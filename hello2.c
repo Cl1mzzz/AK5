76a0a8cb0ed87cf84ad04a27bb0a44ffc1cdab34
Cl1mzzz <vborodiy2004@gmail.com>
2024-12-04 22:10:24 +0200

    first commit

---
+++ hello2.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include "hello1.h"
+
+MODULE_AUTHOR("Volodymyr Borodii");
+MODULE_DESCRIPTION("Hello1, Lab5");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int count = 1;
+module_param(count, int, 0444);
+MODULE_PARM_DESC(count, "Number of times to print 'Hello, world!' message");
+
+static int __init hello2_init(void)
+{
+	if (count == 0 || (count >= 5 && count <= 10)) {
+		pr_warn("Warning: count is 0 or between 5 and 10.\n");
+	} else if (count > 10) {
+		pr_err("Error: count > 10, so module won't load.\n");
+		return -EINVAL;
+	}
+
+	print_hello(count);
+
+	return 0;
+}
+
+static void __exit hello2_exit(void)
+{
+	pr_info("Goodbye, world!\n");
+}
+
+module_init(hello2_init);
+module_exit(hello2_exit);
+
